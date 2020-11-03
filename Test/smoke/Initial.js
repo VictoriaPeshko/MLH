bfb5234ca3aa4599993eec37b6b27ae846baa85d
VictoriaPeshko <peshv@i.ua>
2020-11-02 21:57:49 -0800

    started tests

--- Test/smoke/Initial.js
+++ Test/smoke/Initial.js
@@ -1,3 +1,6 @@
+const sel = require('../../data/selectors.json')
+const exp = require('../../data/expected.json')
+
 describe('My Little Hero', function () { //define suite title by passing a string
 
     describe('Getting to the page', function () { //define sub-suite title by passing a string
@@ -5,7 +8,7 @@ describe('My Little Hero', function () { //define suite title by passing a strin
         it('TC-001 Title is correct ', function () { //define test title by passing a string
             browser.url('https://qa-apps.netlify.app/app_my_hero'); //open baseUrl
             let title = browser.getTitle(); //get page title and assign it to the "title" variable
-            browser.pause(2000); //just pause to visually see that something is happening on the page
+           // browser.pause(2000); //just pause to visually see that something is happening on the page
             expect(title).toEqual('MLH trial'); //compare {title} (actual) and "MLH trial" (expected)
         });
 
@@ -25,28 +28,41 @@ describe('My Little Hero', function () { //define suite title by passing a strin
 
     });
 
-    describe('Elements exist', function () {
+    describe('Labels exist', function () {
 
         it('TC-002 Label for name', function () {
-            const label = $$('.ant-form-item-required')[0].isDisplayed();
+            const label = $$(sel.label)[0].isDisplayed();
             expect(label).toEqual(true);
         });
 
         it('TC-003 Label for gender', function () {
-            const label = $$('.ant-form-item-required')[1].isDisplayed();
+            const label = $$(sel.label)[1].isDisplayed();
             expect(label).toEqual(true);
         });
 
         it('TC-004 Label for age', function () {
-            const label = $$('.ant-form-item-required')[2].isDisplayed();
+            const label = $$(sel.label)[2].isDisplayed();
             expect(label).toEqual(true);
         });
 
         it('TC-005 Label for story', function () {
-            const label = $$('.ant-form-item-required')[3].isDisplayed();
+            const label = $$(sel.label)[3].isDisplayed();
             expect(label).toEqual(true);
         });
 
+    });
+
+    describe('Labeles are correct', function (){
+        it('TC-0008 Label for #1 = What is your Hero\'s name?', function (){
+            let text = $$(sel.label)[0].getAttribute('title')
+            expect(text).toEqual(exp.labelName)
+
+
+        })
+        it('TC-007 Label for gender = Please choose a gender.', function (){
+            let text=$$(sel.label)[1].getText()
+            expect(text).toEqual(exp.labelGender)
+        })
     });
 
 });
